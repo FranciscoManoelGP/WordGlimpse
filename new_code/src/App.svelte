0d4b97c07c1aaebae2de20fbd697e7fbd7563cb5
Francisco Manoel <f.manoel.neto312@gmail.com>
2023-06-21 22:19:27 -0300

    Starting structure update

---
+++ new_code/src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+    import svelteLogo from "./assets/svelte.svg";
+    import viteLogo from "/vite.svg";
+    import Counter from "./lib/Counter.svelte";
+</script>
+
+<header>
+    <h1>PalaVer</h1>
+</header>
+
+<main>
+    <form action="">
+        <input type="text" id="search" />
+        <button>Traduzir</button>
+    </form>
+
+    <article>
+        <section id="carousel">
+            <figure>
+                <img src="" alt="">
+                <figcaption>
+                    <a href="/" target="">Autor: <span>...</span></a>
+                </figcaption>
+            </figure>
+            <figure>
+                <img src="" alt="">
+                <figcaption>
+                    <a href="/" target="">Autor: <span>...</span></a>
+                </figcaption>
+            </figure>
+            <figure>
+                <img src="" alt="">
+                <figcaption>
+                    <a href="/" target="">Autor: <span>...</span></a>
+                </figcaption>
+            </figure>
+        </section>
+
+        <section id="dictionary">
+
+        </section>
+    </article>
+</main>
+
+<style>
+    
+</style>

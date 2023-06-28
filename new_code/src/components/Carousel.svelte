a26f8048cab8547866b34388424efb3b574c983a
Francisco Manoel <f.manoel.neto312@gmail.com>
2023-06-28 09:24:03 -0300

    feat(loading): add loading component to await for API response

--- new_code/src/components/Carousel.svelte
+++ new_code/src/components/Carousel.svelte
@@ -1,21 +1,31 @@
 <script>
     import { onMount } from "svelte";
     import { fetchImagesFromPexels } from "../lib/fetchImagesFromPexels";
+    import Loading from "./Loading.svelte";
 
     export let searchTerm;
     export let images = [];
+    let isLoading = false;
 
-    onMount(async () =>{
+    onMount(async () => {
+        isLoading = true;
         images = await fetchImagesFromPexels(searchTerm);
-    })
+        isLoading = false;
+    });
 </script>
 
 <section id="carousel">
+    {#if isLoading}
+        <Loading />
+    {/if}
+
     {#each images as { src, photographerUrl, photographer }}
         <figure>
-            <img src="{src}" alt="" />
+            <img {src} alt="" />
             <figcaption>
-                <a href="{photographerUrl}" target="">Autor: <span>{photographer}</span></a>
+                <a href={photographerUrl} target=""
+                    >Autor: <span>{photographer}</span></a
+                >
             </figcaption>
         </figure>
     {/each}

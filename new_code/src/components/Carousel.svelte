d720a4316a3dc95af32ad2ccaa4ec8484df54724
Francisco Manoel <f.manoel.neto312@gmail.com>
2023-07-07 16:51:22 -0300

    refactor(components): use {#await} to handle API calls

--- new_code/src/components/Carousel.svelte
+++ new_code/src/components/Carousel.svelte
@@ -1,34 +1,26 @@
 <script>
-    import { onMount } from "svelte";
     import { fetchImagesFromPexels } from "../lib/fetchImagesFromPexels";
     import Loading from "./Loading.svelte";
 
     export let searchTerm;
-    let images = [];
-    let isLoading = false;
-
-    onMount(async () => {
-        isLoading = true;
-        images = await fetchImagesFromPexels(searchTerm);
-        isLoading = false;
-    });
+    const fetchImages = fetchImagesFromPexels(searchTerm);
 </script>
 
-<section id="carousel" >
-    {#if isLoading}
+<section id="carousel">
+    {#await fetchImages}
         <Loading />
-    {/if}
-
-    {#each images as { src, photographerUrl, photographer }}
-        <figure>
-            <img {src} alt="" />
-            <figcaption>
-                <a href={photographerUrl} target=""
-                    >Autor: <span>{photographer}</span></a
-                >
-            </figcaption>
-        </figure>
-    {/each}
+    {:then images}
+        {#each images as { src, photographerUrl, photographer }}
+            <figure>
+                <img {src} alt="" />
+                <figcaption>
+                    <a href={photographerUrl} target="_blank">
+                        Autor: <span>{photographer}</span>
+                    </a>
+                </figcaption>
+            </figure>
+        {/each}
+    {/await}
 </section>
 
 <style>
--- new_code/src/components/Dictionary.svelte
+++ new_code/src/components/Dictionary.svelte
@@ -1,32 +1,24 @@
 <script>
-    import { onMount } from "svelte";
     import { fetchDefinitionsFromDatamuseAPI } from "../lib/fetchDefinitionsFromDatamuseAPI";
     import Loading from "./Loading.svelte";
 
     export let searchTerm;
-    let dictionary = [];
-    let isLoading = false;
-
-    onMount(async () => {
-        isLoading = true;
-        dictionary = await fetchDefinitionsFromDatamuseAPI(searchTerm);
-        isLoading = false;
-    });
+    const fetchDefinitions = fetchDefinitionsFromDatamuseAPI(searchTerm);
 </script>
 
 <section id="dictionary">
-    {#if isLoading}
+    {#await fetchDefinitions}
         <Loading />
-    {/if}
-
-    {#each dictionary as { index, partOfSpeech, description }}
-        <h3>{index}</h3>
-        <p>{partOfSpeech}: {description}</p>
-    {/each}
+    {:then definitions}
+        {#each definitions as { index, partOfSpeech, description }}
+            <h3>{index}</h3>
+            <p>{partOfSpeech}: {description}</p>
+        {/each}
+    {/await}
 </section>
 
 <style>
-    #dictionary{
+    #dictionary {
         overflow-wrap: break-word;
     }
 </style>
